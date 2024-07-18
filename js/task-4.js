37acdb44c06694565679bb83c376eaefc4fc88dd
OleksandrTom <Aleksandrtomkovid@gmail.com>
2024-07-18 13:53:31 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const registerForm = document.querySelector('form');
 
+registerForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  const email = registerForm.elements.email.value.trim();
+  const password = registerForm.elements.password.value.trim();
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const userRegistrationData = {};
+    userRegistrationData.email = email;
+    userRegistrationData.password = password;
+    console.log(userRegistrationData);
+  }
+  form.reset();
+});
