150a5d1501f0828b0fb0471eeda5946485429613
OleksandrTom <Aleksandrtomkovid@gmail.com>
2024-07-23 11:15:37 +0300

    Finish

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector('button');
+const backgroundColorTitle = document.querySelector('.color');
+const bodyColor = document.querySelector('body');
+
+buttonChangeColor.addEventListener('click', () => {
+  backgroundColorTitle.textContent = getRandomHexColor();
+  bodyColor.style.backgroundColor = backgroundColorTitle.textContent;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+//Функція створення нових елементів <div>
+function createBoxes(amount) {
+  let itemArray = [];
+  let increaseValue = 30;
+  for (let i = 0; i < amount; i++) {
+    const item = document.createElement('div');
+    item.classList.add('boxes-item');
+    item.style.backgroundColor = getRandomHexColor();
+    item.style.width = `${increaseValue}px`;
+    item.style.height = `${increaseValue}px`;
+    itemArray.push(item);
+    increaseValue += 10;
+  }
+  colorItemBox.append(...itemArray);
+}
+
+// Функція видалення створених елементів <div>
+function deletNewElements() {
+  colorItemBox.innerHTML = '';
+}
+
+const numInput = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const colorItemBox = document.querySelector('#boxes');
+
+// Додавання елементів <div> у контейнер при кліку на button Create
+btnCreate.addEventListener('click', () => {
+  //Первірка валідності введенного значення
+  if (numInput.value >= 1 && numInput.value <= 100) {
+    // Перевірка наявності елементів у контейнері та, за наявності, їх відалення
+    if (colorItemBox.childNodes.length !== 0) {
+      deletNewElements();
+    }
+    createBoxes(numInput.value);
+  }
+  // Очіщення поля вводу значень
+  numInput.value = '';
+});
+
+// Очіщення результату при кліку на button Destroy
+btnDestroy.addEventListener('click', deletNewElements);
